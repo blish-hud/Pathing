616483e29a7c84b7bbe428838e137e213d9351ab
Dade Lamkins <dade@lamkins.org>
2021-09-11 15:11:59 -0400

    Typo in comment

--- UI/Controls/FlatMap.cs
+++ UI/Controls/FlatMap.cs
@@ -154,7 +154,7 @@ namespace BhModule.Community.Pathing.Entity {
 
             float opacity = MathHelper.Clamp((float)(GameService.Overlay.CurrentGameTime.TotalGameTime.TotalSeconds - _lastMapViewChanged) / 0.65f, 0f, 1f) * 0.8f;
 
-            // TODO: Make this more based on relative vertical access difference.
+            // TODO: Make this more based on relative vertical axis difference.
             var entities = _packState.Entities.ToList().OrderBy(poi => -poi.DrawOrder);
             
             string finalTooltip = string.Empty;
