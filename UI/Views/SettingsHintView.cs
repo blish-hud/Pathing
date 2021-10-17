f2a38d735e1eeeb41dd45decd6f68fc6c72c8c68
Dade Lamkins <dade@lamkins.org>
2021-10-17 19:32:51 -0400

    Fixed min > max for settings hint view.  Bumped manifest version.

--- UI/Views/SettingsHintView.cs
+++ UI/Views/SettingsHintView.cs
@@ -27,7 +27,7 @@ namespace BhModule.Community.Pathing.UI.Views {
                 Parent = buildPanel,
             };
 
-            _bttnOpenSettings.Location = new Point(Math.Min(buildPanel.Width / 2 - _bttnOpenSettings.Width / 2, 20), Math.Min(buildPanel.Height / 2 - _bttnOpenSettings.Height, 20));
+            _bttnOpenSettings.Location = new Point(Math.Max(buildPanel.Width / 2 - _bttnOpenSettings.Width / 2, 20), Math.Max(buildPanel.Height / 2 - _bttnOpenSettings.Height, 20));
 
             _bttnOpenSettings.Click += _bttnOpenSettings_Click;
         }
